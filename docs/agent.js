5e8544299841d427e47b9492778d3a1083d853f8
Oceanos1 <michael.spierer@heig-vd.ch>
2017-10-06 11:17:11 +0200

    change .eslint.js

--- docs/agent.js
+++ docs/agent.js
@@ -1,27 +1,27 @@
-const request = require("superagent");
-const configFile = require("../auth.json");
-const chai = require("chai");
-const mocha = require("mocha");
+const request = require('superagent');
+const configFile = require('../auth.json');
+const chai = require('chai');
+const mocha = require('mocha');
 
-const owner = "lognaume";
-const repo = "HEIGVD-PRO-Project";
-const url = "https://api.github.com/repos/${owner}/${repo}";
+const owner = 'lognaume';
+const repo = 'HEIGVD-PRO-Project';
+const url = 'https://api.github.com/repos/${owner}/${repo}';
 const token = configFile.token;
 const username = configFile.username;
 console.log(token);
 
 const should = chai.should();
 
-describe("the Github API", ()=>{
-    it("allows me to get a list of pull requests", (done) => {
-        request
-            .get(url)
-            .auth(username, token)
-            .set("Accept", "application/vnd.github.v3+json")
-            .end((err, res) => {
-                should.not.exist(err);
-                should.exist(res);
-                done();
-            });
-    });
+describe('the Github API', () => {
+  it('allows me to get a list of pull requests', (done) => {
+    request
+      .get(url)
+      .auth(username, token)
+      .set('Accept', 'application/vnd.github.v3+json')
+      .end((err, res) => {
+        should.not.exist(err);
+        should.exist(res);
+        done();
+      });
+  });
 });
