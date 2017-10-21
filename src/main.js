a8ef3132a0ce96ae942f05c4867a92ed9c7adc6f
Oceanos1 <michael.spierer@heig-vd.ch>
2017-10-22 00:01:36 +0200

    version propre de l'agent

--- src/main.js
+++ src/main.js
@@ -2,19 +2,15 @@ const credentials = require('../auth.json');
 const Agent = require('../src/agent.js');
 const Storage = require('../src/storage.js');
 
-
+// the repo to analyse
 const ownerToAnalyse = 'angular';
 const repoToAnalyse = 'angular.js';
 
-
-// const ownerToAnalyse = 'lognaume';
-// const repoToAnalyse = 'HEIGVD-PRO-Project';
-
+// the repo that will be push
 const ownerToPush = 'edwardransome';
 const repotoPush = 'GithubAnalytics_Client';
 
 const storage = new Storage(ownerToPush, credentials.token, repotoPush);
-
 
 const agent = new Agent(credentials);
 const content = [];
@@ -22,14 +18,15 @@ const content = [];
 agent.fetchAndProcessAllPullRequests(ownerToAnalyse, repoToAnalyse, (err, pullRequests) => {
   const oneDayInMilliSecond = 24 * 60 * 60 * 1000;
 
-  for (const i in pullRequests) {
-    const pullRequest = pullRequests[i];
-    // to calculate the duration of an PR
+  Object.keys(pullRequests).forEach((key) => {
+    const pullRequest = pullRequests[key];
+    // calculation of the duration of an PR
     const dateEnd = (pullRequest.state === 'closed' && pullRequest.merged_at != null) ?
       new Date(pullRequest.merged_at).getTime()
       : new Date().getTime();
 
-    const theDuration = Math.ceil((dateEnd - (new Date(pullRequest.created_at)).getTime()) / (oneDayInMilliSecond));
+    const theDuration = Math.ceil((dateEnd - (
+      new Date(pullRequest.created_at)).getTime()) / (oneDayInMilliSecond));
 
     content.push({
       id: pullRequest.id,
@@ -40,9 +37,8 @@ agent.fetchAndProcessAllPullRequests(ownerToAnalyse, repoToAnalyse, (err, pullRe
       duration: theDuration,
       state: pullRequest.state,
     });
-  }
+  });
 
   storage.publish('docs/json/angular_angular.js.json', JSON.stringify(content), 'new version of data', () => {
   });
-  console.log('done');
 });
